bf5391245254c42dc804e71e3abe1c520214167a
chldnrwo <chldnrwo777@naver.com>
2022-08-17 16:47:43 +0900

    8/17 last commit

---
+++ ch02/ch02/book/IntExample.java
@@ -0,0 +1,20 @@
+package ch02.book;
+
+public class IntExample {
+
+	public static void main(String[] args) {
+		int var1 = 10;
+		int var2 = 012;
+		int var3 = 0xA;
+		int var4 = 010;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(010);
+		
+		//0으로 시작하면 10진수 말고 다른걸로 인식하는듯?
+	}
+
+}
