8134d6ceaa010e7fef32027ab7ac7eb62027c1ab
chldnrwo <chldnrwo777@naver.com>
2022-08-17 13:30:47 +0900

    example

---
+++ java20220817/src/ch01/lecture/Comment.java
@@ -0,0 +1,12 @@
+package ch01.lecture;
+
+public class Comment {
+
+	public static void main(String[] args) {
+		// 주석은 프로그램 실행에 영향을 미치지 않음
+		System.out.println("Hello world!");
+		System.out.println("자바는 쉽지 않다.");
+		System.out.println("잘 배워서 써먹어 보자");
+	}
+
+}
